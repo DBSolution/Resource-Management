6caa500f318c6a5555724797c01bd5fab3c4d5d1
shalith <shalithfernando1@gmail.com>
2015-12-14 19:12:53 +0530

    Initialize the project and create the login with validation.

---
+++ src/connection/DBConnection.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connection;
+
+/**
+ *
+ * @author Asus
+ */
+public interface DBConnection {
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DATABASE_URL = "jdbc:mysql://localhost/resource_management";
+    static final String USERNAME = "root";
+    static final String PASSWORD = "";
+    
+}
